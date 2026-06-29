e28b79d2d6a501655b2302398c9dc63c6a368808
Nidhin Xavier <132472527+NDX7@users.noreply.github.com>
2026-06-29 14:46:50 +0530

    Update pythofuzzer.py

--- pythofuzzer.py
+++ pythofuzzer.py
@@ -1,19 +1,18 @@
 import requests
 
-url="https://nidhin.vercel.app/fuzz"#test url
+base_url=input("Enter the url:")
+url=f"{base_url}/fuzz"
 file=open("wordllists.txt","r")
 r=requests.get(url)
 with open("wordllists.txt","r") as words:
     wordlists=words.read().splitlines()
+session=requests.session()
 for  i in wordlists:
     # print(i)
     test_url=url.replace("fuzz",i)
-    r=requests.get(test_url)
+    r=session.head(test_url)
     if r.status_code in (200,300,301):
         print(test_url)
         
     else:
         continue
-   
-    
-
